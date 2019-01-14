d00f291330e329114a923e99c25255aa17658282
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-01-14 17:02:02 +0000

    test 11 complete,added entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,11 +3,10 @@ class Oystercard
 
 MINIMUM_BALANCE = 1
 MAXIMUM_BALANCE = 90
-attr_reader :balance, :MAXIMUM_BALANCE
+attr_reader :balance, :MAXIMUM_BALANCE, :entry_station
 
-  def initialize
-    @balance = 0
-    @status = false
+  def initialize(balance = 0)
+    @balance = balance
   end
 
   def top_up(amount)
@@ -15,18 +14,18 @@ attr_reader :balance, :MAXIMUM_BALANCE
     @balance += amount
   end
 
-  def touch_in
+  def touch_in(station)
     fail "can't touch in if balance less than #{MINIMUM_BALANCE}" unless got_money?
-    @status = true
+    @entry_station = station
   end
 
   def touch_out
     deduct(MINIMUM_BALANCE)
-    @status = false
+    @entry_station = nil
   end
 
   def in_journey?
-    @status
+    !!@entry_station
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,8 @@
 require 'oystercard'
 
 describe Oystercard do
+  let (:station) {double :station}
+
   it "should have a balance of 0 by default" do
     expect(subject.balance).to eq 0
   end
@@ -12,7 +14,7 @@ describe Oystercard do
 
   it 'deducts fare from my card' do
     subject.top_up(10)
-    subject.touch_in
+    subject.touch_in(station)
     subject.touch_out
     expect(subject.balance).to eq 9
   end
@@ -25,27 +27,37 @@ describe Oystercard do
   end
 
   describe "Card status" do
+    let (:card_with_balance) {Oystercard.new(10)}
+
     it "starts off not in journey" do
       expect(subject).not_to be_in_journey
     end
+    it 'should allow touch in to take an argument' do
+      card_with_balance.touch_in(station)
+      expect(card_with_balance.entry_station).to eq station
+      # expect(subject).to respond_to(:touch_in).with(1).argument
+    end
     it "changes status to in journey when touching in" do
-      subject.top_up(2)
-      subject.touch_in
-      expect(subject).to be_in_journey
+      card_with_balance.touch_in(station)
+      expect(card_with_balance).to be_in_journey
     end
     it "changes status to not in journey when touching out" do
-      subject.top_up(2)
-      subject.touch_in
-      subject.touch_out
-      expect(subject).not_to be_in_journey
+      card_with_balance.touch_in(station)
+      card_with_balance.touch_out
+      expect(card_with_balance).not_to be_in_journey
     end
+    it "should reset the entry station to nil when touching out" do
+      card_with_balance.touch_in(station)
+      card_with_balance.touch_out
+      expect(card_with_balance.entry_station).to eq nil
+    end
+
     it "shouldn't be able to touch in if balance is below minimum balance" do
       subject.top_up(Oystercard::MINIMUM_BALANCE - 0.01)
-      expect{subject.touch_in}.to raise_error "can't touch in if balance less than #{Oystercard::MINIMUM_BALANCE}"
+      expect{subject.touch_in(station)}.to raise_error "can't touch in if balance less than #{Oystercard::MINIMUM_BALANCE}"
     end
     it 'should deduct minimum balance when touching out' do
-      subject.top_up(5)
-      expect {subject.touch_out}.to change{subject.balance}.by(-Oystercard::MINIMUM_BALANCE)
+      expect {card_with_balance.touch_out}.to change{card_with_balance.balance}.by(-Oystercard::MINIMUM_BALANCE)
     end
   end
 
