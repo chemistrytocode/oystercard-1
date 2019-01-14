2e18545f6360db234d3d515116e057bf101638cd
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-01-14 14:49:27 +0000

    Test 6 complete

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,15 @@
 
 class Oystercard
 
-attr_reader :balance
+MAXIMUM_BALANCE = 90
+attr_reader :balance, :MAXIMUM_BALANCE
 
-def initialize
-@balance = 0
-end
+  def initialize
+  @balance = 0
+  end
 
-def top_up(amount)
-  @balance += amount
-end
+  def top_up(amount)
+    fail "can't top up above #{MAXIMUM_BALANCE}" if amount + @balance >  MAXIMUM_BALANCE 
+    @balance += amount
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,7 +6,13 @@ describe Oystercard do
   end
 
   it "can be topped up" do
-    subject.top_up(100)
-    expect(subject.balance).to eq 100
+    subject.top_up(50)
+    expect(subject.balance).to eq 50
   end
+
+  describe "maximum balance" do
+    it "can't be topped up above maximum balance from initiation" do
+    expect{subject.top_up(Oystercard::MAXIMUM_BALANCE + 1)}.to raise_error "can't top up above #{Oystercard::MAXIMUM_BALANCE}"
+  end
+end
 end
