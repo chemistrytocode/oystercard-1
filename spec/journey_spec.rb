9dbb455465b8b220c7d3553c80a6d5f1b02546a8
your name <saule.guzyte@gmail.com>
2019-01-17 16:19:26 +0000

    Changed fare test

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -24,8 +24,8 @@ describe Journey do
       expect(subject.fare).to eq 6
     end
     it 'should return a fare of 2 when travelling between zone 1 & 2' do
-      subject.start_journey(start_station)
-      subject.stop_journey(end_station)
+      allow(subject).to receive(:incomplete_journey?) { false }
+      allow(subject).to receive(:fare_calculator) { 2 }
       expect(subject.fare).to eq 2
     end
   end
